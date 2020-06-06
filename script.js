cdb77621d7e280c0b7dcc9d2dc496413e73d41a6
ugurduyar <ismailugurduyar@gmail.com>
2020-06-07 00:10:03 +0300

    style done, png added

---
+++ script.js
@@ -0,0 +1,14 @@
+const square = document.querySelectorAll(".square");
+const mole = document.querySelectorAll(".mole");
+const timeLeft = document.querySelector("#time-left");
+let score = document.querySelector("#score");
+
+let result = 0;
+
+function randomSquare() {
+  square.forEach((className) => {
+    className.classList.remove("mole");
+  });
+  let randomPosition = square[Math.floor(Math.random() * 9)];
+  randomPosition.classList.add("mole");
+}
